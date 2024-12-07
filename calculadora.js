c9c42f19beabfc80dd26268e71e7e1698d550660
Gerardo SP <ingenierosalcedo@gmail.com>
2024-12-07 13:12:26 -0500

    Feature: Se agrega index para solicitar los datos al usuario y se realizan las validaciones

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,8 @@
+const respuesta = document.getElementById('respuesta')
+const btn = document.querySelector("button")
+
+btn.addEventListener("click", solicitarNumeros)
+
 // Funcion para realizar operaciones
 function realizarOperaciones(num1,num2,operacion){
     if (operacion == "+"){
@@ -6,13 +11,29 @@ function realizarOperaciones(num1,num2,operacion){
         return num1 - num2
     }else if (operacion == "*"){
         return num1 * num2
-    }else if (operacion == "/"){
-        if (num2 != 0){
-            return num1 * num2
-        }else{
-            console.log('No se puede dividir entre cero');
-        }
+    }else if (operacion == "/" && num2 != 0){
+            return num1 / num2
     }else{
-        console.log('No se puede efectuar la operación, revise los datos ingresados');
+        alert('No se puede efectuar la operación, revise los datos ingresados');
     }
 }
+
+//Funcion para solicitar valores al usuario
+function solicitarNumeros(){
+    let operacion=''
+    while(operacion != "salir"){
+        operacion=prompt("Ingrese el simbolo de la operacion que desea realizar \n sumar: + \n restar: - \n multiplicar: * \n dividir: / \nPara terminar escriba: salir").toLowerCase()
+        if (operacion == 'salir'){
+            alert('Muchas gracias, ¡Hasta pronto!')
+        }else if(operacion == '+'||operacion == '-'||operacion == '*'|| operacion == '/'){
+            numero1=parseFloat(prompt("Digite el primer número: "));
+            numero2=parseFloat(prompt("Digite el segundo número: "));
+            let resultado = realizarOperaciones(numero1,numero2,operacion)
+            alert(`El resultado de ${numero1} ${operacion} ${numero2} es : ${resultado}`);
+            respuesta.textContent = `El resultado de ${numero1} ${operacion} ${numero2} es : ${resultado}`;
+        }else{
+            alert('Ingrese una opción valida!')
+        }
+    }
+}
+
